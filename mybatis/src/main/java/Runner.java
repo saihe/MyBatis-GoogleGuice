7613c4828e5c06099ac7a23c0eb81fedd27e40ea
FBC_saito <saito@fubright.co.jp>
2019-11-27 13:01:51 +0900

    指定した行数ごと（１００行と記述）にバルクインサートを行うよう修正

--- mybatis/src/main/java/Runner.java
+++ mybatis/src/main/java/Runner.java
@@ -8,33 +8,45 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Runner {
     public void run() {
+        int dividePoint = 100;
         EmployeeDao employeeDao = DaoUtil.getDao(EmployeeDao.class);
         try {
             List<Employee> employeeList = new ArrayList<>();
-            try (
-                    Stream<String> stream = Files.lines(
-                            Paths.get(
-                                    this.getClass().getResource("import/employee.csv").toURI())
+            try(
+                    Stream<Employee> stream = Files.lines(
+                            Paths.get(this.getClass().getResource("import/employee.csv").toURI())
                             , StandardCharsets.UTF_8
+                    ).skip(1).map(row ->
+                            new Employee(
+                                    null
+                                    , row.split(Pattern.quote(","))[1]
+                                    , row.split(Pattern.quote(","))[2]
+                                    , row.split(Pattern.quote(","))[3]
+                                    , row.split(Pattern.quote(","))[4]
+                            )
                     )
             ) {
-                employeeList = stream.skip(1).map(row -> new Employee(
-                        null
-                        , row.split(Pattern.quote(","))[1]
-                        , row.split(Pattern.quote(","))[2]
-                        , row.split(Pattern.quote(","))[3]
-                        , row.split(Pattern.quote(","))[4]
-                )).collect(Collectors.toList());
+                AtomicInteger loopCounter = new AtomicInteger(1);
+                stream.forEach(employee -> {
+                    employeeList.add(employee);
+                    // 分割点の倍数の場合インサートする
+                    if (loopCounter.get() % dividePoint == 0) {
+                        System.out.println("インサート行番号" + loopCounter);
+                        employeeDao.insetList(employeeList);
+                        employeeList.clear();
+                    }
+                    loopCounter.set(loopCounter.incrementAndGet());
+                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            employeeDao.insetList(employeeList);
         } catch (Exception e) {
             e.printStackTrace();
         }
